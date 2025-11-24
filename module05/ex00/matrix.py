d51d543525a9ecbe496a1044ba697dd7712906c5
Diego Agudelo <dagudelo@f1br4s7.clusters.42paris.fr>
2025-11-24 17:13:49 +0100

    Push

--- module05/ex00/matrix.py
+++ module05/ex00/matrix.py
@@ -1,12 +1,12 @@
 import sys
 import copy
+import numpy as np
 
 class Matrix:
     data : list[list]
     shape : tuple
 
     def ft_create_zero_matrix(self, data):
-        print('case 1')
         values = data[0]
         rows = values[0]
         columns = values[1] - 1
@@ -18,7 +18,6 @@ class Matrix:
             self.data.append(to_add)  
 
     def ft_create_matrix(self, data):
-        print('case 2')
         self.data = copy.copy(data[0])
 
     def ft_check_rows_of_data(self, data):
@@ -66,7 +65,6 @@ class Matrix:
                 sys.exit(1)
         self.shape = (rows, max_columns)
 
-
     def __init__(self, *data):
         self.data = []
         choice = self.ft_take_a_choice(data)
@@ -83,13 +81,24 @@ class Matrix:
         self.ft_define_shape_of_data()
 
     def __str__(self):
-        return f"data: {self.data}, shape: {self.shape}"
+        return "[" + "\n ".join(str(row) for row in self.data) + "]"
 
-    def __add__(self):
-        [...]
+    def __add__(self, other):
+        
+        if not isinstance(other, list[list]):
+            raise ValueError('List[] type')
+
+
+        result = self.data + other
+
+        # for i in 
+
+
+
+        return result
     
-    def __radd__(self):
-        [...]
+    def __radd__(self, other):
+        return self.__add__(other)
     
     def __sub__(self):
         [...]
@@ -115,16 +124,38 @@ class Matrix:
     def T(self):
         [...]
 
-    def shape(self):
-        [...]
+    # def shape(self):
+    #     return f"{self.shape}"
 
 def main():
     print('-----------Case shape-----------')
-    m1 = Matrix((5, 3))
-    print(m1)
+    case1 = (5, 3)
+    m1_alpha = Matrix(case1)
+    m1 = np.zeros(case1)
+
+    print(f"Custom function: \n{m1_alpha}, shape {m1_alpha.shape}")
+    print(f"Original function: \n{m1}, shape {m1.shape}")
     print('-----------Case lists -----------')
-    m2 = Matrix([[1, 2, 4], [4, 3, 1]])
-    print(m2)
+    case2 = [[1, 2, 4], [4, 3, 1]]
+    m2_alpha = Matrix(case2)
+    m2 = np.array(case2, np.float32)
+    print(f"Custom function: \n {m2_alpha}")
+    print(f"Original function: \n {m2}")
+
+    print('-----------Case 3 -----------')
+    case3 = [[1, 2, 4], [4, 3, 1]]
+    m3_alpha = Matrix(case3)
+    m3 = np.array(case3, np.float32)
+    print(f"Custom function: \n {m3_alpha}")
+    print(f"Original function: \n {m3}")
+
+    
+    m4 = m2 + m3
+
+    print(f"Result sum m2 and m3:c sum \n{m4}")
+
+    m4_alpha =  m2_alpha + m3_alpha
+    print(f"Result custom sum m2 and m3: \n{m4_alpha}")
 
 if __name__ == "__main__":
     main()
